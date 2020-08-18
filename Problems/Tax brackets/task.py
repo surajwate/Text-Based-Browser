351bee2a9b50a1e7d10a111872fd3f49e747e761
Suraj Wate <suraj.wate@gmail.com>
2020-08-18 20:35:15 +0530

    Tax brackets

---
+++ Problems/Tax brackets/task.py
@@ -0,0 +1,15 @@
+income = int(input())
+percent = 0
+
+if 0 <= income <= 15527:
+	percent = 0
+elif 15528 <= income <= 42707:
+	percent = 15
+elif 42708 <= income <= 132406:
+	percent = 25
+elif income >= 132407:
+	percent = 28
+
+calculated_tax = income * percent / 100
+
+print(f"The tax for {income} is {percent}%. That is {round(calculated_tax)} dollars!")
