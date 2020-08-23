2cd6f63e1816525be6516d9fbda3f7da6e9be1b8
Suraj Wate <suraj.wate@gmail.com>
2020-08-24 02:28:07 +0530

    get status_code

--- Problems/Success or fail/task.py
+++ Problems/Success or fail/task.py
@@ -1,9 +1,13 @@
+"""Define a function check_success(url) that, given a URL, sends out a GET request and returns "Success" if it
+succeeds and "Fail" otherwise.
+
+"""
 import requests
 
 
 def check_success(url):
     resp = requests.get(url)
-    if 200 <= resp.status_code < 400:
+    if 200 <= resp.status_code < 400:  # check the value of status code
         return 'Success'
     else:
         return 'Fail'
